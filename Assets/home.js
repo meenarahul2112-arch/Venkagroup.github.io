46cb37834ce6b0891d70b5f8a52b3a40e1db242e
meenarahul2112-arch <meena.rahul2112@gmail.com>
2026-03-05 16:21:14 +0100

    Update home.js

---
+++ Assets/home.js
@@ -0,0 +1,18 @@
+<div class="slideshow">
+  <img id="slide" src="Assets/Images/group-photo1.jpg">
+</div>
+
+<script>
+const images = [
+  "Assets/Images/group-photo1.jpg",
+  "Assets/Images/group-photo2.jpg",
+  "Assets/Images/group-photo3.jpg"
+];
+
+let index = 0;
+
+setInterval(() => {
+  index = (index + 1) % images.length;
+  document.getElementById("slide").src = images[index];
+}, 3000);
+</script>
