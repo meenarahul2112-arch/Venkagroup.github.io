6ffcd724689e1eea91a99c1d8e09fade4811f111
meenarahul2112-arch <meena.rahul2112@gmail.com>
2026-03-12 17:07:38 +0100

    Update home.js

--- Assets/home.js
+++ Assets/home.js
@@ -17,6 +17,31 @@ setInterval(() => {
 
 
 
+const pubImages = [
+  "Assets/Images/TOC_pub1.png",
+  "Assets/Images/TOC_pub2.png",
+  "Assets/Images/TOC_pub3.png",
+  "Assets/Images/TOC_pub4.png",
+  "Assets/Images/TOC_pub5.png",
+  "Assets/Images/TOC_pub6.png",
+  "Assets/Images/TOC_pub7.png",
+  "Assets/Images/TOC_pub8.png",
+  "Assets/Images/TOC_pub9.png",
+  "Assets/Images/TOC_pub10.png",
+  "Assets/Images/TOC_pub11.png",
+  "Assets/Images/TOC_pub12.png"
+];
+
+let pubIndex = 0;
+
+setInterval(() => {
+  pubIndex = (pubIndex + 1) % pubImages.length;
+  document.getElementById("slide-pub").src = pubImages[pubIndex];
+}, 3000);
+
+
+
+
 
 
 
